0d7dd30dd8f0297e360eaf80c41f4343580311b6
KdntNinja <kaidentninja@gmail.com>
2023-12-03 11:28:48 +0000

    error fixes

--- api/index.py
+++ api/index.py
@@ -4,8 +4,10 @@ from magic8ball.magic8ball import magic8ball_bp
 from chatroom.chatroom import chat_room_bp
 
 from flask import Flask
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_bytes(32)
 
 app.register_blueprint(welcome_page_bp)
 app.register_blueprint(chat_room_bp, url_prefix="/chatroom")
